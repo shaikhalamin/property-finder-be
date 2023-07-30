decd31f941335b7906cecf79f002f26b2ecfca33
Shaikh Al Amin <alamin.cse15@gmail.com>
2023-07-31 02:50:12 +0600

    swagger docs load issue fixing

--- src/main.ts
+++ src/main.ts
@@ -46,8 +46,15 @@ async function bootstrap() {
 
   SwaggerModule.setup('api-docs', app, document, {
     swaggerOptions: {
-      customJs:
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.0.0/swagger-ui-bundle.js',
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+      ],
+      customCssUrl: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+      ],
     },
   });
 
